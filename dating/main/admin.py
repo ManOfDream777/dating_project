18fbb0b0bf66a6b335fed11b9c670f125d4f7e16
Nikita <yolomyls@gmail.com>
2023-06-29 13:29:12 +0300

    сделал первый пункт задания

---
+++ dating/main/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MyUser
+from .forms import MyUserCreationForm, MyUserChangeForm
+
+class MyUserAdmin(UserAdmin):
+    model = MyUser
+    add_form = MyUserCreationForm
+    list_display = ('email', 'last_name', 'first_name',)
+    ordering = ('last_name', 'first_name',)
+    fieldsets = (
+        (None, {"fields": ('email', 'first_name', 'last_name', 'gender', 'photo', 'password')}),
+        (
+            ("Права доступа"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Даты"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ('last_name', 'first_name', 'email', 'gender', 'photo', 'password1', 'password2'),
+            },
+        ),
+    )
+    form = MyUserChangeForm
+    # change_password_form = AdminPasswordChangeForm
+    list_filter = ("last_name", "gender", "is_staff", "groups")
+    search_fields = ("first_name", "last_name", "email", "gender")
+    ordering = ("last_name", "first_name",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+admin.site.register(MyUser, MyUserAdmin)
---
+++ dating/main/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import MyUserManager
+
+class MyUser(AbstractUser, models.Model):
+    gender_choices = (
+        ('M', ("Мужчина")),
+        ('F', ("Женщина"))
+    )
+    
+    username = None
+    first_name = models.CharField(verbose_name='Имя', max_length=150)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=150)
+    email = models.EmailField(verbose_name='Email', unique=True)
+    gender = models.CharField(choices=gender_choices, max_length=7, verbose_name='Пол')
+    photo = models.ImageField(verbose_name='Аватарка', upload_to='user_photos/')
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['last_name', 'first_name', 'gender',]
+
+    objects = MyUserManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('first_name', 'last_name')
+
+    def get_full_name(self) -> str:
+        return f'{self.last_name} {self.first_name[0]}'
